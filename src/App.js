137f8740c9e0e34d497cbb671534c54f5513df8e
Tomas Smilgius <tsmilgius@gmail.com>
2021-09-17 15:21:24 +0300

    added router withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-     <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage}/> 
+        <Route path='/shop/hats' component={HatsPage}/>
+      </Switch> 
     </div>
   );
 }
