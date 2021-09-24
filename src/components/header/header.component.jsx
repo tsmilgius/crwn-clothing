5039852b34004ea48b18c6ba408b156b0a88b51f
Tomas Smilgius <tsmilgius@gmail.com>
2021-09-24 16:30:10 +0300

    added redux and cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
-    </Link>
+    </Link> 
     <div className='options'>
       <Link className='option' to='/shop'>SHOP</Link>
       <Link className='option' to='/shop'>CONTACT</Link>
       {
         currentUser ? 
-        <div class='option' onClick={()=>auth.signOut()}>SIGN OUT</div> :
+        <div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div> :
         <Link className='option' to='/signin'>SIGN IN</Link>
       }
-    </div>  
+      <CartIcon/>
+    </div>
+    {
+      hidden ? null :
+      <CartDropdown/> 
+    }
+     
   </div>
-)
+);
 
-export default Header;
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({ 
+  currentUser,
+  hidden
+})
+
+export default connect(mapStateToProps)(Header);
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/signin/signin.component';
-
+import SignUp from '../../components/sign-up/sign-up.component';
 import './sign-in-and-sign-up.styles.scss';
 
+
 const SignInAndSignUpPage = () => (
- <div>
+ <div className='sign-in-and-sign-up'>
    <SignIn/>
+   <SignUp/>
  </div>    
 )
 
